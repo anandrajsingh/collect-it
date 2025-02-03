2a8ad55ad8b7d999a508ba47a0e84e9edc9372ae
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-02-03 13:40:57 +0530

    resend mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,11 +3,17 @@ import { Resend } from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 
+if (!process.env.RESEND_API_EMAIL) {
+    throw new Error('RESEND_API_EMAIL is not set')
+}
+
+const resendMail = process.env.RESEND_API_EMAIL
+
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmationLink = `http://localhost:3000/new-verification?token=${token}`
 
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: resendMail,
         to: email,
         subject: 'Confirm your email',
         html: `<p>Click <a href="${confirmationLink}">here</a> to confirm your email</p>`
@@ -18,7 +24,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     const confirmationLink = `http://localhost:3000/new-password?token=${token}`
 
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: resendMail,
         to: email,
         subject: 'Reset Link',
         html: `<p>Click <a href="${confirmationLink}">here</a> to reset password</p>`
